a677dcd70e5e75f1b6b9189b9fcb24e28451d2a3
mahammadmustafayev <mustafamehmed1251@gmail.com>
2024-09-29 12:13:27 +0400

    added main

--- seleniumProject/main.js
+++ seleniumProject/main.js
@@ -9,10 +9,7 @@ async function runTest() {
         .build();
 
     try {
-
-        await driver.get('https://www.linkedin.com/');
-        let signIn= await driver.findElement(By.className('nav__button-secondary btn-secondary-emphasis btn-md'));
-        await signIn.click();
+        await driver.get('https://www.linkedin.com/search/results/people/?keywords=back%20end%20developer&origin=SWITCH_SEARCH_VERTICAL');
         let emailInput= await driver.findElement(By.name('session_key'));
         let passwordInput= await driver.findElement(By.name('session_password'));
         await emailInput.click();
@@ -22,14 +19,51 @@ async function runTest() {
         let signInBtn= await driver.findElement(By.className('btn__primary--large from__button--floating'));
         await signInBtn.click();
 
-        let searchInput= await driver.findElement(By.css('[data-view-name="search-global-typeahead-input"]'));
-        await searchInput.click();
-        await searchInput.sendKeys('back end developer',Key.ENTER);
+        let peopleData= await driver.executeScript(`
+            const people = [...document.querySelectorAll('button')].filter(button => 
+                button.textContent.trim() === 'Connect'
+            ).slice(0, 5).map(button => {
+                const personElement = button.closest('.entity-result__item');
+                const name = personElement.querySelector('.entity-result__title-text').textContent.trim();
+                const title = personElement.querySelector('.entity-result__primary-subtitle').textContent.trim();
+                const location = personElement.querySelector('.entity-result__secondary-subtitle').textContent.trim();
+                
+                return {
+                    name,
+                    title,
+                    location
+                };
+            });
+            return people;
+            `);
+        peopleData.forEach(person=>{
+
+        });
+
+
+        // await driver.get('https://www.linkedin.com/');
+        // let popupBtn=await driver.findElement(By.className('cta-modal__dismiss-btn absolute h-4 w-4 p-1 top-2 right-2 hover:cursor-pointer focus:outline focus:outline-2 focus:outline-color-action'));
+        // await popupBtn.click();
+        // let signIn= await driver.findElement(By.className('nav__button-secondary btn-secondary-emphasis btn-sm ml-3'));
+        // await signIn.click();
+        // let emailInput= await driver.findElement(By.name('session_key'));
+        // let passwordInput= await driver.findElement(By.name('session_password'));
+        // await emailInput.click();
+        // await emailInput.sendKeys('mustafamehmed1251@gmail.com');
+        // await passwordInput.click();
+        // await passwordInput.sendKeys('A1251n?mz');
+        // let signInBtn= await driver.findElement(By.className('btn__primary--large from__button--floating'));
+        // await signInBtn.click();
+
+        // let searchInput= await driver.findElement(By.css('[data-view-name="search-global-typeahead-input"]'));
+        // await searchInput.click();
+        // await searchInput.sendKeys('back end developer',Key.ENTER);
 
         // let peopleBtn= await driver.findElement(By.css('button[aria-pressed="false"]'));
         // await peopleBtn.click();
-        let test= "https://www.linkedin.com/search/results/people/?keywords=back%20end%20developer&origin=SWITCH_SEARCH_VERTICAL";
-        await driver.get(test);
+        // let test= "https://www.linkedin.com/search/results/people/?keywords=back%20end%20developer&origin=SWITCH_SEARCH_VERTICAL";
+
+        // await driver.get(test);
        
         // let searcBox= await driver.findElement(By.id("web"));
         // await searcBox.click();
